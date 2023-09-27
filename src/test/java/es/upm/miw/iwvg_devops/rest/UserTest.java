bdc5a0e38db730a9a9dfc89a1063ad26d5377e3a
jinyifan <563503448@qq.com>
2023-09-27 20:28:52 +0200

    enhanced UserTest #9

--- src/test/java/es/upm/miw/iwvg_devops/rest/UserTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/UserTest.java
@@ -16,6 +16,7 @@ public class UserTest {
     @BeforeEach
     public void setUp() {
         List<Fraction> fractions = new ArrayList<>();
+        user = new User();
         fractions.add(new Fraction(1, 2));
         fractions.add(new Fraction(3, 4));
         user = new User("1234", "Joe", "Swift", fractions);
@@ -34,7 +35,11 @@ public class UserTest {
     public void testGetFamilyName(){
         assertEquals("Swift", user.getFamilyName());
     }
-
+    @Test
+    void testAddFraction(){
+        user.addFraction(new Fraction(2,1));
+        assertEquals(3,user.getFractions().size());
+    }
     @Test
     public void testGetFullName(){
         assertEquals("Joe Swift", user.fullName());
@@ -67,5 +72,12 @@ public class UserTest {
         newFractions.add(new Fraction(1, 3));
         user.setFractions(newFractions);
         assertEquals(1, user.getFractions().size());
+    }
+    @Test
+    void testToString(){
+        List<Fraction> newFractions = new ArrayList<>();
+        newFractions.add(new Fraction(2,4));
+        User user = new User("1", "Lola", "Smith", newFractions);
+        assertEquals("User{id='1', name='Lola', familyName='Smith', fractions=[Fraction{numerator=2, denominator=4}]}",user.toString());
     }
 }
