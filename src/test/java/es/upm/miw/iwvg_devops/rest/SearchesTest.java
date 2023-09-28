3ef71564087fca03ddf276c7f9c7ed43138b12d4
jinyifan <563503448@qq.com>
2023-09-29 00:33:03 +0200

    added search c #12

--- src/test/java/es/upm/miw/iwvg_devops/rest/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/SearchesTest.java
@@ -21,4 +21,11 @@ public class SearchesTest {
         assertEquals(List.of("Fernandez","Blanco","López","Blanco","Torres"), new Searches().findUserFamilyNameBySomeImproperFraction()
                 .collect(Collectors.toList()));
     }
+
+    @Test
+    void testFindDecimalFractionByUserName(){
+        assertEquals(List.of(0.0, 1.0,2.0,0.2,-0.5,0.5,1.0), new Searches().findDecimalFractionByUserName("Oscar")
+                .collect(Collectors.toList()));
+    }
+
 }
