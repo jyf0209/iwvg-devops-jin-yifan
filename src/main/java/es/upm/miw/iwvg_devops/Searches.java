a2436c9085f2e1268eaa885e6d811ffe0d2eab8f
jinyifan <563503448@qq.com>
2023-09-29 00:28:59 +0200

    added search 8 #11

--- src/main/java/es/upm/miw/iwvg_devops/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/Searches.java
@@ -1,5 +1,7 @@
 package es.upm.miw.iwvg_devops;
 
+import java.util.stream.Stream;
+
 public class Searches{
     public Fraction findFirstProperFractionByUserId(String id){
         return new UsersDatabase().findAll()
@@ -7,5 +9,11 @@ public class Searches{
                 .flatMap(user -> user.getFractions().stream()
                         .filter(Fraction::isProper)).findFirst()
                 .orElse(null);
+    }
+    public Stream<String> findUserFamilyNameBySomeImproperFraction(){
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getFractions().stream()
+                        .anyMatch(Fraction::isImproper))
+                .map(User::getFamilyName);
     }
 }
